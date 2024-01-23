b15e9402121a5059e671e6190779c9f0b758ac14
Tigran <tshagharyant20@gmail.com>
2024-01-23 18:40:42 +0400

    in pogress

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,9 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
 const List = () => {
     const [todos, setTodos] = useState([]);
     const [page, setPage] = useState(1);
     const [limit, setLimit] = useState(10);
+    const parentRef = useRef();
 
         const getTodos = (page,limit) => {
             fetch(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}&_page=${page}`)
@@ -21,10 +22,10 @@ const List = () => {
                             <span>{todo.id} </span>
                             <span key={todo.id} > {todo.title}</span>
                         </div>
-
                     )
                 }
             )}
+            <div style={{height:20, background: 'green'}} />
         </div>
     );
 };
---
+++ src/myHooks/useScroll.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UseScroll = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default UseScroll;
